10f1d1087afd377cd281348c5a10a4012ab1493f
Axel-342 <axel.mouisson@gmail.com>
2023-12-13 10:43:44 +0100

    1.7

--- front/js/campaigns.js
+++ front/js/campaigns.js
@@ -3,7 +3,7 @@ const queryString_url_id = window.location.search
 const urlSearchParams = new URLSearchParams(queryString_url_id)
 const _id = urlSearchParams.get('_id')
 
-const promiseProduct = fetch(`http://localhost:3000/api/products/${_id}`)
+const promisecampaign = fetch(`http://localhost:3000/api/campaigns/${_id}`)
 .then((response) => response.json())
 .then((response) => { 
   meubleData = response
@@ -65,12 +65,12 @@ const promiseProduct = fetch(`http://localhost:3000/api/products/${_id}`)
   
 //Récupérer les données séléctionnées par l'utilisateur afin de les envoyer vers le panier
 //Récupération des données de l'API :
-fetch('http://localhost:3000/api/products')
+fetch('http://localhost:3000/api/campaigns')
   .then((res) => res.json())
   .then((res) => {
     data = res
     //Récupération, au sein de l'API, du produit séléctionné par l'utilisateur 
-    const selectedProduct_id = data.find((element) => element._id === _id)
+    const selectedcampaign_id = data.find((element) => element._id === _id)
 
     let idForm = document.querySelector('#colors')
     let quantity = document.querySelector('#quantity')
@@ -79,45 +79,45 @@ fetch('http://localhost:3000/api/products')
     /*LE ADD EVENT LISTENER*/
     //Ecouter le click, puis envoyer l'article vers le panier
     btn_addToCart.addEventListener('click', (event) => {
-         for(j = 0 ; j < selectedProduct_id.colors.length ; j ++)
+         for(j = 0 ; j < selectedcampaign_id.colors.length ; j ++)
       event.preventDefault()
       //Mettre le choix de l'utilisateur dans une variable
       const choixForm = idForm.value
       //Mettre la quantité dans une variable
       let choixQuantity = quantity.value
       //Récupération des valeurs au sein de la variables "optionsProduit"
-      optionsProduit = {
-        name: selectedProduct_id.name,
-        selectedProduct_id: selectedProduct_id._id,
+      optionscampagne = {
+        name: selectedcampaign_id.name,
+        selectedProduct_id: selectedcampaign_id._id,
         option_produit: choixForm,
         quantite: choixQuantity,
-        image: selectedProduct_id.imageUrl,
+        image: selectedcampaign_id.imageUrl,
       }
       //Nous stockons les éventuels résultats provenants du localStorage dans fromJSON
       fromJSON = localStorage.getItem('idJSON')
       //Fonction servant à ajouter un article au panier 
       function addToArray() {
-        fromJS.push(optionsProduit)
+        fromJS.push(optionscampagne)
         localStorage.setItem('idJSON', JSON.stringify(fromJS))
-        window.alert("Le produit vient d'être ajouté à votre panier")
+        window.alert("La campagne vient d'être ajouté à votre panier")
       }
       
       //Fonction servant à retourner les produits ayant le même ID, afin que le tri soit fait lors de l'ajout au panier
-      function sameId(product) {
-        return product.selectedProduct_id === optionsProduit.selectedProduct_id
+      function sameId(campaign) {
+        return campaign.selectedcampaign_id === optionscampagne.selectedcampaign_id
       }
       
       ///LA GESTION DU PANIER///
       if (fromJSON) {
         fromJS = JSON.parse(fromJSON) //On ajoute dans le tableau JS les produits déjà présents dans le storage
         for (i = 0; i < fromJS.length; i++){
-          if (optionsProduit.quantite === '0') {
+          if (optionscampagne.quantite === '0') {
             alert('Le quantité séléctionnée est de 0')
           } else if (
-            optionsProduit.selectedProduct_id !== fromJS[i].selectedProduct_id
+            optionscampagne.selectedcampaign_id !== fromJS[i].selectedcampaign_id
           ) {
-            const quantityToNumber = parseInt(optionsProduit.quantite)
-            optionsProduit.quantite = quantityToNumber
+            const quantityToNumber = parseInt(optionscampagne.quantite)
+            optionscampagne.quantite = quantityToNumber
             let foundSameID = fromJS.find(sameId)
             if (foundSameID === undefined) {
               // Si undefined nous est retourné, c'est que l'ID n'est présent sur aucune ligne du tableau
@@ -125,55 +125,55 @@ fetch('http://localhost:3000/api/products')
               break //Puis nous sortons de la boucle for
             }
           } else if (
-            optionsProduit.selectedProduct_id === fromJS[i].selectedProduct_id
+            optionscampagne.selectedcampaign_id === fromJS[i].selectedcampaign_id
           ) {
             //Si l'ID du produit séléctionné est présent sur la ligne en cours d'analyse
             let foundSameID = fromJS.find(sameId) // Alors nous utilisons la méhode find afin de retourner le premier élément ayant le même ID
-            if (foundSameID.option_produit === optionsProduit.option_produit) {// Si cet élément possède la même option produit
-              let quantityToNumber = parseInt(optionsProduit.quantite) // Nous convertissons la quantite du produit sélectionné en nombre
+            if (foundSameID.option_campagne === optionscampagne.option_campagne) {// Si cet élément possède la même option produit
+              let quantityToNumber = parseInt(optionscampagne.quantite) // Nous convertissons la quantite du produit sélectionné en nombre
               let quantityToNumberBis = parseInt(foundSameID.quantite) // Nous convertissons la quantite du produit retourné par la méthode find en nombre
               foundSameID.quantite = quantityToNumber += quantityToNumberBis // Nous additionnons les deux quantités ci-dessus afin d'obtenir le nouveau nombre total de produit avec le même ID / même options
               localStorage.setItem('idJSON', JSON.stringify(fromJS)) //Pour finir, nous mettons à jour le localStorage
               break // Puis nous sortons de la boucle for
             } else if (
-              foundSameID.option_produit !== optionsProduit.option_produit
+              foundSameID.option_campagne !== optionscampagne.option_campagne
             ) {
               //Si le produit retourné par find possède bien le même ID, mais avec une option différente :
               const filtrage = fromJS.filter(
-                (produit) =>
-                  produit.selectedProduct_id ===
-                  optionsProduit.selectedProduct_id,
+                (campagne) =>
+                  campagne.selectedcampaign_id ===
+                  optionscampagne.selectedcampaign_id,
               ) //Alors on déclare la variable filtrage, qui utilise la méthode filter pour vérifier si un d'autres produits avec le même ID existe à travers le tableau fromJS
               const filtrageFindOption = filtrage.find(
                 (element) =>
-                  element.option_produit === optionsProduit.option_produit,
+                  element.option_campagne === optionscampagne.option_campagne,
               ) //Puis on applique de nouveau la méthode find au sein de filtrage, pour vérifier si un élément avec la même option nous est retourné
               if (filtrageFindOption === undefined) {
                 // Si aucun produit correspondant n'est trouvé :
-                const toNumber = parseInt(optionsProduit.quantite)
-                optionsProduit.quantite = toNumber
+                const toNumber = parseInt(optionscampagne.quantite)
+                optionscampagne.quantite = toNumber
                 addToArray() // Alors nous ajoutons le produit au panier
               } else if (
-                filtrageFindOption.option_produit ===
-                optionsProduit.option_produit
+                filtrageFindOption.option_campagne ===
+                optionscampagne.option_campagne
               ) {
                 // Si find nous retourne un élément :
-                let convertQuantity = parseInt(optionsProduit.quantite) // Alors nous convertissons en number la quantite du produit séléctionné
+                let convertQuantity = parseInt(optionscampagne.quantite) // Alors nous convertissons en number la quantite du produit séléctionné
                 let convertQuantityBis = parseInt(filtrageFindOption.quantite) // Puis nous faisons de même avec la quantite de l'élément retourné par find
                 filtrageFindOption.quantite = convertQuantity += convertQuantityBis // Nous additionnons les 2 number ci-dessus afin d'obtegnir la quantité total
                 localStorage.setItem('idJSON', JSON.stringify(fromJS)) // Puis nous mettons le localStorage à jour
               }
               break //Pour sortir de la boucle for, afin d'éviter une répétition pour chaque ligne du tableau
             }
-          }
+          }Pro
         }
       } else {
-        if (optionsProduit.quantite === '0') {
+        if (optionscampagne.quantite === '0') {
           alert('Le quantité séléctionnée est de 0')
         } else {
           fromJS = []
-          const toNumber = parseInt(optionsProduit.quantite)
-          optionsProduit.quantite = toNumber
+          const toNumber = parseInt(optionscampagne.quantite)
+          optionscampagne.quantite = toNumber
           addToArray()
         }
       }
@@ -184,10 +184,10 @@ fetch('http://localhost:3000/api/products')
 
 // Recuperation du prix produit dans l'API
 
-const priceFetching = fetch('http://localhost:3000/api/products')
+const priceFetching = fetch('http://localhost:3000/api/campaigns')
   .then((res) => res.json())
   .then((res) => {
     data = res
-    let fetchedProduct = data.find((element) => element._id === _id)
-    document.getElementById('price').innerHTML = fetchedProduct.price 
+    let fetchedcampaign = data.find((element) => element._id === _id)
+    document.getElementById('price').innerHTML = fetchedcampaign.price 
   })
--- front/js/script.js
+++ front/js/script.js
@@ -4,15 +4,15 @@ const items = (document.getElementById('items'))
 /* Nous initialisons la fonction fetchProduct, fonction asynchrone servant à récupérer les données présentes 
 dans l'API, puis à les convertir au format Javascript 
   Les données au format Javascript sont ensuite stockées dans la variable meubleData*/
-const fetchProduct = async () => {
-  await fetch('http://localhost:3000/api/products')
+const fetchcampaign = async () => {
+  await fetch('http://localhost:3000/api/campaigns')
     .then((response) => response.json())
     .then((response) => { 
       meubleData = response
       return meubleData
     })
 }
-fetchProduct()
+fetchcampaign()
 
 /*Une fois la fonction fetchProduct exécutée, nous déclarons displayProduct, fonction asynchrone qui attend 
   l'éxecution de fetchProduct avant de s'éxecuter
@@ -20,19 +20,19 @@ fetchProduct()
   qui s'y trouvent
   Cela nous permet ensuite d'injecter les données correspondantes au sein du HTML 
   Pour finir, nous appliquons la méthode .join(), afin de supprimer les apostrophres présentes entre chaque carte produit */
-const displayProduct = async () => {
-  await fetchProduct()
+const displaycampaign = async () => {
+  await fetchcampaign()
   items.innerHTML = meubleData // tab[prod, prod2, prod3]
     .map(                      //      0       1    2
       (meuble) =>
-        `<a href="./product.html?_id=${meuble._id}">
+        `<a href="./campaign.html?_id=${meuble._id}">
         <article class = "article">
         <img class="item__img" src = "${meuble.imageUrl}" alt = "${meuble.altTxt}" />
-        <h3 class="productName">${meuble.name}</h3>
-        <p class="productDescription">${meuble.description}</p>
+        <h3 class="campaignName">${meuble.name}</h3>
+        <p class="campaignDescription">${meuble.description}</p>
     </article>
     </a>`,
     )
     .join(' ') 
 }
-displayProduct()
+displaycampaign()
